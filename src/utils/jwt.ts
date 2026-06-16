7157ac569cc5d5a2a957fb785a2c34d05c66e2fc
AlexIn1309 <moran.graciano.cesar@gmail.com>
2026-06-15 20:03:31 -0600

    adding jwt utils

---
+++ src/utils/jwt.ts
@@ -0,0 +1,24 @@
+import { SignJWT, VerifyJWT } from "jose";
+import { JwtPayloadDto } from "../dtos/auth/jwt-payloads";
+
+export async function generateJwt(
+  payload: JwtPayloadDto,
+  secret: string,
+): Promise<string> {
+  const secretKey = new TextEncoder().encode(secret);
+
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("15m")
+    .sign(secretKey);
+}
+
+export async function verifyAccessToken(
+  token: string,
+  secret: string,
+): Promise<JwtPayloadDto> {
+  const secretKey = new TextEncoder().encode(secret);
+  const { payload } = await new VerifyJWT(token, secretKey);
+  return payload as JwtPayloadDto;
+}
